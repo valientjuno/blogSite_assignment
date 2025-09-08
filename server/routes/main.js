f4a62ea2e0d1178b5134fc179841772dd3daa608
valientjuno <jessedoake91@gmail.com>
2025-09-08 10:36:36 -0600

    fixed layouts main.ejs
    removed extra anchor close tag and corrected spacing

--- server/routes/main.js
+++ server/routes/main.js
@@ -5,22 +5,14 @@ const cookieParser = require("cookie-parser");
 
 // Home Page
 router.get("/", async (req, res) => {
+  const locals = {
+    title: "NodeJS Blog",
+    description:
+      "A Blog template application that will be used for your own use.",
+  };
+
   try {
-    const locals = {
-      title: "NodeJS Blog",
-      description:
-        "A Blog template application that will be used for your own use.",
-    };
-    let searchTerm = req.body.searchTerm;
-    const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z]/g, "");
-
-    const data = await Post.find({
-      $or: [
-        { title: { $regex: new RegExp(searchNoSpecialChar, "i") } },
-        { body: { $regex: new RegExp(searchNoSpecialChar, "i") } },
-      ],
-    });
-
+    const data = await Post.find().sort({ title: "desc" });
     res.render("index", { locals, data });
   } catch (error) {
     console.log(error);
@@ -28,30 +20,5 @@ router.get("/", async (req, res) => {
 });
 
 // get post by ID
-router.get("/post/:id", async (req, res) => {});
-
-// search route
-router.get("/", async (req, res) => {
-  try {
-    const locals = {
-      title: "NodeJS Blog",
-      description:
-        "A Blog template application that will be used for your own use.",
-    };
-    let searchTerm = req.body.SearchTerm;
-    const searchNoSpecialChar = searchtTerm.replace(/[^a-zA-Z]/g, "");
-
-    const data = await Post.find({
-      $or: [
-        { title: { $regex: new RegExp(searchNoSpecialChar, "i") } },
-        { body: { $regex: new RegExp(searchNoSpecialChar, "i") } },
-      ],
-    });
-
-    res.render("index", { locals, data });
-  } catch (error) {
-    console.log(error);
-  }
-});
 
 module.exports = router;
