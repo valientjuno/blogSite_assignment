459374edb9d290eaf7749cbf751867a50349bef8
valientjuno <jessedoake91@gmail.com>
2025-09-08 13:56:01 -0600

    5th commit
    close to complete with video guided script build in 2/3 lessons in course.

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
-const cookieParser = require("cookie-parser");
-
 // Home Page
 router.get("/", async (req, res) => {
   const locals = {
@@ -12,13 +10,59 @@ router.get("/", async (req, res) => {
   };
 
   try {
-    const data = await Post.find().sort({ title: "desc" });
-    res.render("index", { locals, data });
+    const data = await Post.find().sort({ createdAt: "desc" });
+    res.render("index", {
+      locals,
+      data,
+      currentPage: page,
+      nextPage: hasNextPage ? nextPage : null,
+      previousPage: hasNextPagePlus ? previousPage : null,
+    });
   } catch (error) {
     console.log(error);
   }
 });
 
 // get post by ID
+router.post("/search", async (req, res) => {
+  try {
+    let slug = req.params.id;
+
+    const data = await Post.findById({ _id: slug });
+
+    const locals = {
+      title: data.title,
+      description:
+        "A Blog template application that will be used for your own use.",
+    };
+    res.render("post", (locals, data));
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// search
+router.post("/search", async (req, res) => {
+  try {
+    const locals = {
+      title: "Search",
+      description: "A blog template made with NodeJS and ExpressJS",
+    };
+
+    let searchTerm = req.body.SerchTerm;
+    const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z]/g, "");
+
+    const data = await Post.find({
+      $or: [
+        { title: { $regex: new RegExp(searchNoSpecialChar, "i") } },
+        { body: { $regex: new RegExp(searchNoSpecialChar, "i") } },
+      ],
+    });
+
+    res.render("search", { locals, data });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = router;
